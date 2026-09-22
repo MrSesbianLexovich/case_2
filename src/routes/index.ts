9589fab666195b4d24044573147e82516b795f86
MrSesbianLexovich <fishersasha122@gmail.com>
2026-09-22 11:32:07 +0300

    fixed  routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import equipmentRouter from "./equipmentRoutes.ts";
 import requestsRouter from "./requestsRoutes.ts";
+import healthRouter from "./healthRoutes.ts";
 
 const router = Router()
 
 router.use('/equipment', equipmentRouter)
 router.use('/requests', requestsRouter)
+router.use('/health', healthRouter)
 
 export default router
