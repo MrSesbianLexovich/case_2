9c313bdace590e49ebaadcb4ca2ffd671a6f3c9e
MrSesbianLexovich <fishersasha122@gmail.com>
2026-09-22 03:32:48 +0300

    added validation in equipmentRouter

--- src/routes/equipmentRoutes.ts
+++ src/routes/equipmentRoutes.ts
@@ -1,34 +1,37 @@
 import { Router } from "express";
 import { validate } from "../utils/validator";
 import { asyncHandler } from "../utils/asyncHandler";
+import { equipmentPartialSchema, equipmentQuerySchema, equipmentSchema, idSchema } from "../types/schemas";
 
 const equipmentRouter = Router()
 
 equipmentRouter.get("/",
+    validate(equipmentQuerySchema,'query')    
 )
 
 equipmentRouter.post("/", 
-
+    validate(equipmentSchema, 'body')
 )
 
 equipmentRouter.get("/:id",
-
+    validate(idSchema, "params")
 )
 
 equipmentRouter.patch("/:id",
-
+    validate(idSchema, 'params'),
+    validate(equipmentPartialSchema,'body')
 )
 
 equipmentRouter.delete("/:id",
-
+    validate(idSchema, 'params')
 )
 
 equipmentRouter.get("/:id/requests",
-
+    validate(idSchema, 'params')
 )
 
 equipmentRouter.get("/:id/weather",
-
+    validate(idSchema, 'params')
 )
 
 export default equipmentRouter
