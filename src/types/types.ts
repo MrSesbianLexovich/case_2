6f8fa7e7d18dae2efa40f847e4b84b28ee2e8276
MrSesbianLexovich <fishersasha122@gmail.com>
2026-09-22 10:09:24 +0300

    added RequestQuery type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,11 +1,14 @@
 import type { z } from "zod";
-import { equipmentQuerySchema, equipmentSchema, requestRepoSchema } from "./schemas";
+import { equipmentQuerySchema, equipmentSchema, requestQuerySchema, requestRepoSchema } from "./schemas";
 
 export type EquipmentQuery = z.infer<typeof equipmentQuerySchema>;
 
 export type EquipmentWithId = z.infer<typeof equipmentSchema> & {
     id: string
 }
+
+
+export type RequestsQuery = z.infer<typeof requestQuerySchema>
 
 export type RequestRepo = z.infer<typeof requestRepoSchema>
 
